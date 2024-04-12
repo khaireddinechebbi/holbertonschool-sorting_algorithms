5a4299799e0552e8ddcdceeda3c300d855cd437b
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-04-12 11:43:28 +0100

    0 bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+*/
+void bubble_sort(int *array, size_t size)
+{
+size_t i, j;
+int num;
+
+if (array == NULL || size < 2)
+return;
+
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+num = array[j];
+array[j] = array[j + 1];
+array[j + 1] = num;
+print_array(array, size);
+}
+}
+}
+}
